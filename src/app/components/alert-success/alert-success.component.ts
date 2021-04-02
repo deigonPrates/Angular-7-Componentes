a162d5c70f5ac79225f0da25573d2952da929023
Deigon Prates <deigon_prates@hotmail.com>
2021-04-02 15:05:31 -0300

    Criando componentes inlines

--- src/app/components/alert-success/alert-success.component.ts
+++ src/app/components/alert-success/alert-success.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'alert-success',
-  templateUrl: './alert-success.component.html',
-  styleUrls: ['./alert-success.component.css']
+  template: `<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <ng-content></ng-content>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+            </div>`
 })
 export class AlertSuccessComponent implements OnInit {
 
