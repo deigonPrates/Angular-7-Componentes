42c9697aeaeeb24195056e4eb9041f8320b4a3e1
Deigon Prates <deigon_prates@hotmail.com>
2021-04-03 14:20:03 -0300

    Usando ngOnInit no componente modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -27,7 +27,10 @@ export class ModalComponent implements OnInit {
   constructor(private element:ElementRef) { }
 
   ngOnInit(): void {
-    console.log(this.element.nativeElement);
+    const nativeElement: HTMLElement = this.element.nativeElement;
+    nativeElement.querySelector('[modal-title]').classList.add('modal-title');
+    nativeElement.querySelector('[modal-body]').classList.add('modal-body');
+    nativeElement.querySelector('[modal-footer]').classList.add('modal-footer');
   }
 
   hide(){
